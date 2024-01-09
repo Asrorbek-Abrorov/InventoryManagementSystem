27c4cc87045415362e62591f1738a9eb85aa2905
Asrorbek Abrorov <138248626+Asrorbek-Abrorov@users.noreply.github.com>
2024-01-09 15:10:10 +0500

    MainUi and Program.cs

--- InventoryManagementSystem/Program.cs
+++ InventoryManagementSystem/Program.cs
@@ -1,59 +1,16 @@
-﻿using InventoryManagementSystem.Displays;
+﻿using InventoryManagementSystem;
+using InventoryManagementSystem.Displays;
 
-namespace InventoryManagementSystem;
-
-class Program
+namespace InventoryManagementSystem
 {
-    static void Main(string[] args)
+    class Program
     {
-        var productManager = new ProductManager("/home/as_abrorov/RiderProjects/InventoryManagementSystem/InventoryManagementSystem/Database/products.txt");
-        var productManagerUi = new ProductManagerUi(productManager);
-        productManagerUi.Run();
-        var supplierManager = new SupplierManager("/home/as_abrorov/RiderProjects/InventoryManagementSystem/InventoryManagementSystem/Database/suppliers.txt");
-
-        var supplier1 = new Supplier()
+        static void Main(string[] args)
         {
-            Name = "Supplier1",
-            ContactDetails = "123456789",
-            Products = new List<Product>()
-            {
-                productManager.GetProductDetails(1)
-            }
-        };
-
-        var supplier2 = new Supplier()
-        {
-            Name = "Supplier2",
-            ContactDetails = "123456789",
-            Products = new List<Product>()
-            {
-                productManager.GetProductDetails(2)
-            }
-        };
-
-        var product1 = new Product
-        {
-            Name = "Product 1",
-            Description = "Description 1",
-            Price = 10,
-            StockQuantity = 100,
-            Supplier = supplier1
-        };
-
-        productManager.AddProduct(product1);
-
-        var product2 = new Product()
-        {
-            Name = "Product 2",
-            Description = "Description 2",
-            Price = 20,
-            StockQuantity = 200,
-            Supplier = supplier2
-        };
-
-        productManager.AddProduct(product2);
-
-        supplierManager.AddSupplier(supplier1);
-        supplierManager.AddSupplier(supplier2);
+            var productManager = new ProductManager("/home/as_abrorov/RiderProjects/InventoryManagementSystem/InventoryManagementSystem/Database/products.txt");
+            var supplierManager = new SupplierManager("/home/as_abrorov/RiderProjects/InventoryManagementSystem/InventoryManagementSystem/Database/suppliers.txt");
+            var mainUi = new MainUi(productManager, supplierManager);
+            mainUi.Run();
+        }
     }
 }
--- InventoryManagementSystem/Uis/MainUi.cs
+++ InventoryManagementSystem/Uis/MainUi.cs
@@ -1,6 +1,56 @@
-namespace InventoryManagementSystem.Uis;
+using InventoryManagementSystem.Displays;
+using Spectre.Console;
 
-public class MainUi
+namespace InventoryManagementSystem
 {
-    
+    public class MainUi
+    {
+        private readonly ProductManagerUi _productManagerUi;
+        private readonly SupplierManagerUi _supplierManagerUi;
+
+        public MainUi(ProductManager productManager, SupplierManager supplierManager)
+        {
+            _productManagerUi = new ProductManagerUi(productManager);
+            _supplierManagerUi = new SupplierManagerUi(supplierManager);
+        }
+
+        public void Run()
+        {
+            bool keepRunning = true;
+            while (keepRunning)
+            {
+                AnsiConsole.Clear();
+                AnsiConsole.Write(
+                    new FigletText("* Main Menu *")
+                        .LeftJustified()
+                        .Color(Color.Red1));
+                var choice = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                        .Title("[green]*** Choose an option ***[/]?")
+                        .PageSize(10)
+                        .AddChoices(new[]
+                        {
+                            "Products", "Suppliers",
+                            "Back"
+                        }));
+                switch (choice)
+                {
+                    case "Products":
+                        _productManagerUi.Run();
+                        break;
+
+                    case "Suppliers":
+                        _supplierManagerUi.Run();
+                        break;
+
+                    case "Back":
+                        keepRunning = false;
+                        break;
+                }
+                AnsiConsole.WriteLine();
+                AnsiConsole.WriteLine("Enter to continue...");
+                Console.ReadKey();
+            }
+        }
+    }
 }
