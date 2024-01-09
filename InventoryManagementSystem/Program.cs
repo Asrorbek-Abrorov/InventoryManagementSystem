f0a944d49d249fa2ef42068968cd8e5543da56a8
Asrorbek Abrorov <138248626+Asrorbek-Abrorov@users.noreply.github.com>
2024-01-09 14:20:48 +0500

    Program.cs

--- InventoryManagementSystem/Program.cs
+++ InventoryManagementSystem/Program.cs
@@ -5,24 +5,30 @@ class Program
     static void Main(string[] args)
     {
         var productManager = new ProductManager("/home/as_abrorov/RiderProjects/InventoryManagementSystem/InventoryManagementSystem/Database/products.txt");
-        
+
         var supplierManager = new SupplierManager("/home/as_abrorov/RiderProjects/InventoryManagementSystem/InventoryManagementSystem/Database/suppliers.txt");
 
         var supplier1 = new Supplier()
         {
             Name = "Supplier1",
             ContactDetails = "123456789",
-            Products = [productManager.GetProductDetails(1)]
+            Products = new List<Product>()
+            {
+                productManager.GetProductDetails(1)
+            }
         };
-        
+
         var supplier2 = new Supplier()
         {
             Name = "Supplier2",
             ContactDetails = "123456789",
-            Products = [productManager.GetProductDetails(2)]
+            Products = new List<Product>()
+            {
+                productManager.GetProductDetails(2)
+            }
         };
-        
-        var pruduct1 = new Product
+
+        var product1 = new Product
         {
             Name = "Product 1",
             Description = "Description 1",
@@ -30,10 +36,10 @@ class Program
             StockQuantity = 100,
             Supplier = supplier1
         };
-        
-        // productManager.AddProduct(pruduct1);
-        
-        var pruduct2 = new Product()
+
+        productManager.AddProduct(product1);
+
+        var product2 = new Product()
         {
             Name = "Product 2",
             Description = "Description 2",
@@ -41,12 +47,10 @@ class Program
             StockQuantity = 200,
             Supplier = supplier2
         };
-        
-        
-        
-        // productManager.UpdateProduct(pruduct2, 1);
+
+        productManager.AddProduct(product2);
+
         supplierManager.AddSupplier(supplier1);
         supplierManager.AddSupplier(supplier2);
     }
 }
-
