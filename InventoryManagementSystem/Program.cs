ab607fedfec7d1e4cdc4c8b16a787fe54e9b74c9
Asrorbek Abrorov <138248626+Asrorbek-Abrorov@users.noreply.github.com>
2024-01-09 14:52:31 +0500

    Program.cs

--- InventoryManagementSystem/Program.cs
+++ InventoryManagementSystem/Program.cs
@@ -1,11 +1,14 @@
-﻿namespace InventoryManagementSystem;
+﻿using InventoryManagementSystem.Displays;
+
+namespace InventoryManagementSystem;
 
 class Program
 {
     static void Main(string[] args)
     {
         var productManager = new ProductManager("/home/as_abrorov/RiderProjects/InventoryManagementSystem/InventoryManagementSystem/Database/products.txt");
-
+        var productManagerUi = new ProductManagerUi(productManager);
+        productManagerUi.Run();
         var supplierManager = new SupplierManager("/home/as_abrorov/RiderProjects/InventoryManagementSystem/InventoryManagementSystem/Database/suppliers.txt");
 
         var supplier1 = new Supplier()
