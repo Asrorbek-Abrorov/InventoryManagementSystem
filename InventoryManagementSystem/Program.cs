a576e4c8255690b126f6e38207ad778f297c7220
Asrorbek Abrorov <138248626+Asrorbek-Abrorov@users.noreply.github.com>
2024-01-09 08:02:44 +0500

    Program.cs

--- InventoryManagementSystem/Program.cs
+++ InventoryManagementSystem/Program.cs
@@ -7,6 +7,20 @@ class Program
         var productManager = new ProductManager("/home/as_abrorov/RiderProjects/InventoryManagementSystem/InventoryManagementSystem/Database/products.txt");
         
         var supplierManager = new SupplierManager("/home/as_abrorov/RiderProjects/InventoryManagementSystem/InventoryManagementSystem/Database/suppliers.txt");
+
+        var supplier1 = new Supplier()
+        {
+            Name = "Supplier1",
+            ContactDetails = "123456789",
+            Products = [productManager.GetProductDetails(1)]
+        };
+        
+        var supplier2 = new Supplier()
+        {
+            Name = "Supplier2",
+            ContactDetails = "123456789",
+            Products = [productManager.GetProductDetails(2)]
+        };
         
         var pruduct1 = new Product
         {
@@ -14,14 +28,10 @@ class Program
             Description = "Description 1",
             Price = 10,
             StockQuantity = 100,
-            Supplier = new Supplier
-            {
-                Name = "Supplier",
-                ContactDetails = "ContactDetails"
-            }
+            Supplier = supplier1
         };
         
-        productManager.AddProduct(pruduct1);
+        // productManager.AddProduct(pruduct1);
         
         var pruduct2 = new Product()
         {
@@ -29,17 +39,14 @@ class Program
             Description = "Description 2",
             Price = 20,
             StockQuantity = 200,
-            Supplier = new Supplier
-            {
-                Name = "Supplier updated",
-                ContactDetails = "ContactDetails updated"
-            }
+            Supplier = supplier2
         };
         
         
         
-        productManager.UpdateProduct(pruduct2, 1);
-
+        // productManager.UpdateProduct(pruduct2, 1);
+        supplierManager.AddSupplier(supplier1);
+        supplierManager.AddSupplier(supplier2);
     }
 }
 
