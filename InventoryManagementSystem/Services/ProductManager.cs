74644244440d5d96fed66613e25a981cb0fb9420
Asrorbek Abrorov <138248626+Asrorbek-Abrorov@users.noreply.github.com>
2024-01-09 14:52:20 +0500

    About products

--- InventoryManagementSystem/Services/ProductManager.cs
+++ InventoryManagementSystem/Services/ProductManager.cs
@@ -11,17 +11,20 @@ public class ProductManager
         this.productsFilePath = productsFilePath;
     }
 
-    public void AddProduct(Product product)
+    public bool AddProduct(Product product)
     {
         using (var writer = new StreamWriter(productsFilePath, true))
         {
             writer.WriteLine($"{product.Id},{product.Name},{product.Description},{product.Price},{product.StockQuantity},{product.Supplier.Id}");
         }
+        return true;
     }
 
-    public void UpdateProduct(Product product, int id)
+    public bool UpdateProduct(Product product, int id)
     {
         var products = File.ReadAllLines(productsFilePath).ToList();
+
+        bool productUpdated = false;
 
         for (int i = 0; i < products.Count; i++)
         {
@@ -29,22 +32,35 @@ public class ProductManager
             if (int.Parse(productParts[0]) == id)
             {
                 products[i] = $"{product.Id},{product.Name},{product.Description},{product.Price},{product.StockQuantity},{product.Supplier.Id}";
+                productUpdated = true;
                 break;
             }
         }
 
-        File.WriteAllLines(productsFilePath, products);
-    }
+        if (productUpdated)
+        {
+            File.WriteAllLines(productsFilePath, products);
+        }
 
-    public void RemoveProduct(int productId)
+        return productUpdated;
+    }
+    public bool RemoveProduct(int productId)
     {
         var products = File.ReadAllLines(productsFilePath).ToList();
 
+        bool productRemoved = false;
+
         products.RemoveAll(p => p.StartsWith($"{productId},"));
 
-        File.WriteAllLines(productsFilePath, products);
-    }
+        if (products.Count < int.Parse(File.ReadAllLines(productsFilePath).Length.ToString()))
+        {
+            File.WriteAllLines(productsFilePath, products);
+            productRemoved = true;
+        }
 
+        return productRemoved;
+    }
+    
     public Product GetProductDetails(int productId)
     {
         var products = File.ReadAllLines(productsFilePath);
@@ -59,13 +75,59 @@ public class ProductManager
                 Description = productParts[2],
                 Price = decimal.Parse(productParts[3]),
                 StockQuantity = int.Parse(productParts[4]),
-                Supplier = new Supplier
-                {
-                    Id = int.Parse(productParts[5])
-                }
+                Supplier = GetSupplierById(int.Parse(productParts[5]))
             };
         }
 
         return null;
     }
+    public Supplier GetSupplierById(int supplierId)
+    {
+        var suppliers = File.ReadAllLines("/home/as_abrorov/RiderProjects/InventoryManagementSystem/InventoryManagementSystem/Database/suppliers.txt").ToList();
+
+        foreach (var supplierLine in suppliers)
+        {
+            var supplierParts = supplierLine.Split(',');
+            var supplier = new Supplier
+            {
+                Id = int.Parse(supplierParts[0]),
+                Name = supplierParts[1],
+                ContactDetails = supplierParts[2]
+            };
+
+            if (supplier.Id == supplierId)
+            {
+                return supplier;
+            }
+        }
+
+        return null;
+    }
+    public List<Product> GetAllProducts()
+    {
+        var products = new List<Product>();
+
+        using (var reader = new StreamReader(productsFilePath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var productParts = line.Split(',');
+                var product = new Product
+                {
+                    Id = int.Parse(productParts[0]),
+                    Name = productParts[1],
+                    Description = productParts[2],
+                    Price = decimal.Parse(productParts[3]),
+                    StockQuantity = int.Parse(productParts[4]),
+                    Supplier = GetSupplierById(int.Parse(productParts[5]))
+                };
+
+                products.Add(product);
+            }
+        }
+
+        return products;
+    }
+    
 }
--- InventoryManagementSystem/Uis/ProductManagerUi.cs
+++ InventoryManagementSystem/Uis/ProductManagerUi.cs
@@ -1,6 +1,182 @@
-namespace InventoryManagementSystem.Uis;
+using Spectre.Console;
+using InventoryManagementSystem;
 
-public class ProductManagerUi
+namespace InventoryManagementSystem.Displays
 {
-    
+    public class ProductManagerUi
+    {
+        private readonly ProductManager productManager;
+
+        public ProductManagerUi(ProductManager productManager)
+        {
+            this.productManager = productManager;
+        }
+
+        public void Run()
+        {
+            AnsiConsole.Clear();
+            bool keepRunning = true;
+            while (keepRunning)
+            {
+                AnsiConsole.Clear();
+                AnsiConsole.Write(
+                    new FigletText("* Product Manager *")
+                        .LeftJustified()
+                        .Color(Color.Red1));
+                var choice = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                        .Title("[green]*** Products ***[/]?")
+                        .PageSize(10)
+                        .AddChoices(new[]
+                        {
+                            "Create", "Update", "Delete",
+                            "View product", "View all products",
+                            "Back"
+                        }));
+                switch (choice)
+                {
+                    case "Create":
+                        var product = CreateProduct();
+                        var check = productManager.AddProduct(product);
+                        AnsiConsole.WriteLine(check ? "Product created successfully" : "Something went wrong!");
+                        break;
+
+                    case "Update":
+                        int id = AnsiConsole.Ask<int>("ID : ");
+                        product = productManager.GetProductDetails(id);
+
+                        if (product != null)
+                        {
+                            product = UpdateProduct();
+                            check = productManager.UpdateProduct(product, id);
+                            if (check)
+                            {
+                                AnsiConsole.WriteLine("Product updated successfully!");
+                            }
+                            else
+                            {
+                                AnsiConsole.WriteLine("Product not found!");
+                            }
+                        }
+                        else
+                        {
+                            AnsiConsole.WriteLine("Product not found");
+                        }
+                        break;
+
+                    case "Delete":
+                        id = AnsiConsole.Ask<int>("ID : ");
+                        check = productManager.RemoveProduct(id);
+                        if (check)
+                        {
+                            AnsiConsole.WriteLine("Product deleted successfully!");
+                        }
+                        else
+                        {
+                            AnsiConsole.WriteLine("Product not found!");
+                        }
+                        break;
+
+                    case "View product":
+                        id = AnsiConsole.Ask<int>("ID : ");
+                        product = productManager.GetProductDetails(id);
+
+                        if (product is not null)
+                        {
+                            AnsiConsole.WriteLine("******************************");
+                            AnsiConsole.WriteLine($"Id : {product.Id}");
+                            AnsiConsole.WriteLine($"Name : {product.Name}");
+                            AnsiConsole.WriteLine($"Description : {product.Description}");
+                            AnsiConsole.WriteLine($"Price : {product.Price}");
+                            AnsiConsole.WriteLine($"Stock Quantity : {product.StockQuantity}");
+                            AnsiConsole.WriteLine($"Supplier : {product.Supplier.Name}");
+                            AnsiConsole.WriteLine("******************************");
+                        }
+                        else
+                        {
+                            AnsiConsole.WriteLine("Product not found");
+                        }
+
+                        break;
+
+                    case "View all products":
+                        var products = productManager.GetAllProducts();
+                        foreach (var prod in products)
+                        {
+                            AnsiConsole.WriteLine("******************************");
+                            AnsiConsole.WriteLine($"Id : {prod.Id}");
+                            AnsiConsole.WriteLine($"Name : {prod.Name}");
+                            AnsiConsole.WriteLine($"Description : {prod.Description}");
+                            AnsiConsole.WriteLine($"Price : {prod.Price}");
+                            AnsiConsole.WriteLine($"Stock Quantity : {prod.StockQuantity}");
+                            AnsiConsole.WriteLine($"Supplier : {prod.Supplier.Name}");
+                        }
+                        break;
+
+                    case "Back":
+                        keepRunning = false;
+                        break;
+                }
+                AnsiConsole.WriteLine();
+                AnsiConsole.WriteLine("Enter to continue...");
+                Console.ReadKey();
+            }
+        }
+
+        public Product CreateProduct()
+        {
+            AnsiConsole.Clear();
+            AnsiConsole.WriteLine("*** Creating ***");
+            AnsiConsole.WriteLine();
+
+            var name = AnsiConsole.Ask<string>("Name : ").Trim();
+
+            var description = AnsiConsole.Ask<string>("Description : ").Trim();
+
+            var price = AnsiConsole.Ask<decimal>("Price : ");
+
+            var stockQuantity = AnsiConsole.Ask<int>("Stock Quantity : ");
+
+            var supplierId = AnsiConsole.Ask<int>("Supplier ID : ");
+
+            var product = new Product
+            {
+                Name = name,
+                Description = description,
+                Price = price,
+                StockQuantity = stockQuantity,
+                Supplier = new Supplier { Id = supplierId }
+            };
+
+            return product;
+        }
+
+        public Product UpdateProduct()
+        {
+            AnsiConsole.Clear();
+            AnsiConsole.WriteLine("*** Updating ***");
+            AnsiConsole.WriteLine();
+
+            var name = AnsiConsole.Ask<string>("New Name : ").Trim();
+
+            var description = AnsiConsole.Ask<string>("New Description : ").Trim();
+
+            var price = AnsiConsole.Ask<decimal>("New Price : ");
+
+            var stockQuantity = AnsiConsole.Ask<int>("New Stock Quantity : ");
+
+            var supplierId = AnsiConsole.Ask<int>("New Supplier ID : ");
+
+            var product = new Product
+            {
+                Name = name,
+                Description = description,
+                Price = price,
+                StockQuantity = stockQuantity,
+                Supplier = new Supplier { Id = supplierId }
+            };
+
+            return product;
+        }
+    }
 }
